f2d8fef58f0fd666ec8d408e64fbb683f089217c
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2024-01-30 23:29:04 +0530

    create verifytoken file

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js"
+import blogRoute from "./routes/blog.js"
+import userRoute from "./routes/users.js"
+import cookieParser from "cookie-parser";
 
 const app  = express()
 dotenv.config()
@@ -16,7 +19,24 @@ const connect = async () => {
 };
 
 //middleware
-app.use("/auth" , authRoute);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth" , authRoute);
+app.use("/api/blogs" , blogRoute);
+app.use("/api/users" , userRoute);
+
+
+app.use((err,req,res,next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something Went Wrong!"
+  return res.status(errorStatus).json(errorMessage).json({
+    success:false,
+    status:errorStatus,
+    message:errorMessage,
+    stack:err.stack,
+  });
+});
 
 app.listen(8800, () => {
     connect()
